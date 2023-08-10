acf704b8916a6730ad3d7341072e8e5a21a3432d
Nélio Espíndula Junior <nelioespindula.junior@gmail.com>
2023-08-09 22:05:02 -0300

    Update FollowPlayer.cs
    Remove unused function

--- Unit 1 - Player Control/Scripts/FollowPlayer.cs
+++ Unit 1 - Player Control/Scripts/FollowPlayer.cs
@@ -7,12 +7,6 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
