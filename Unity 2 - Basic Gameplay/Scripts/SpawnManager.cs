ad7c907cc6db3f9ff95cc53cfbf52828d76a89d1
Nélio Espíndula Junior <nelioespindula.junior@gmail.com>
2023-08-09 22:05:58 -0300

    Update SpawnManager.cs
    Remove unused functions

--- Unity 2 - Basic Gameplay/Scripts/SpawnManager.cs
+++ Unity 2 - Basic Gameplay/Scripts/SpawnManager.cs
@@ -15,11 +15,6 @@ public class SpawnManager : MonoBehaviour
     {
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
-
-    void Update()
-    {
-
-    }
     
     void SpawnRandomAnimal() {
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
